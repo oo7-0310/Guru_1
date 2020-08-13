3eae29c6326826a880f373aa7888920dbdc51446
oo7-0310 <63403642+oo7-0310@users.noreply.github.com>
2020-08-14 00:27:04 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            int lastTick = 0;
+            while (true)
+            {
+                int currentTick = System.Environment.TickCount;
+                int elapsedTick = currentTick - lastTick;
+                //입력
+
+                //로직
+
+                //렌더링
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Hello World!");
+            }
+
+            
+        }
+    }
+}
